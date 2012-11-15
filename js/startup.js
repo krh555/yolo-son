1a874c8494e2d38f09db0d1688c9938a4fdfeef9
Kevin Hines <krhines@live.unc.edu>
2012-11-15 18:14:53 -0500

    Add comments + TODO

--- js/startup.js
+++ js/startup.js
@@ -4,22 +4,42 @@ $(document).ready( function () {
 	$('#addComment').hide();
 	gMapInit();
 	
+	/* TODO
+	* -Fix error handling for login/register ajax requests 
+	* -Hide form after authenticating
+	* -Add logout button to account bar and make handler
+	* -Fix greeting after logging in 
+	* -LONG TERM: Build account bar for authenticatd users i.e. list of liked stories, users, etc.
+	* 
+	*/
+	
+	
 	//Register button event handler sends registration request to users.php
 	$('#register').click( function() {
+		//Get form input from #loginForm in #account div
 		var name = $('#userName').val();
 		var pw = $('#password').val();
+		//MD5 encrypt password before sending it to the server
 		pw = CryptoJS.MD5(pw).toString();
-		$.post("php/users.php", { userName: name, password: pw, register: "yes" }, function(data) {
+		//Called users.php script with register action to create a new account
+		// !!! FIX ERROR HANDLING HERE AND IN users.php !!!
+		$.post("php/users.php", { userName: name, password: pw, action: "register" }, function(data) {
 			$('#account').append(data);
-		} );
+		}).error(function(data) {
+			$('#account').append(data);	
+		});
 	});
 	
 	//Submit login form button event handler sends login request to users.php
 	$('#submitLogin').click( function() {
+		//Get form input from #loginForm in #account div
 		var name = $('#userName').val();
 		var pw = $('#password').val();
+		//MD5 encrypt password before sending it to the server
 		pw = CryptoJS.MD5(pw).toString();
-		$.post("php/users.php", { userName: name, password: pw, login: "yes" }, function(data) {
+		//Call users.php with login action set to authenticate existing user account
+		// !!! FIX ERROR HANDLING HERE AND IN users.php !!!
+		$.post("php/users.php", { userName: name, password: pw, action: "login" }, function(data) {
 			$('#account').append(data);
 		} );
 	});
