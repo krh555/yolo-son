bbd610d8b5bf635225d6a856a1b39f85fa8ada95
Kevin Hines <krhines@live.unc.edu>
2012-12-05 16:17:18 -0500

    Check user_id and display like/flag or edit/delete buttons appropriately in news feed

--- Views/Story.js
+++ Views/Story.js
@@ -25,11 +25,17 @@ fillStories = function( stories ){
 		$new_story.append( '<p>' + story.location + '</p>' );
 		$new_story.append( '<p>' + story.likes + ' likes</p>' );
 		$new_story.append( '<p>' + story.num_comments + ' comments</p>' );
-		$new_story.append( '<button class="editStory" value="' + story.id +'" > Edit</button>' );
 		$new_story.append( '<button class="comment" value="' + story.id +'" > Comments </button>' );
-		$new_story.append( '<button class="like" value="' + story.id +'" > Like </button>' );
-		$new_story.append( '<button class="flag" value="' + story.id +'" > Mark as inappropriate </button>' );
-		
+		if( current_user != 0){
+			if(story.user_id == current_user.id){
+				$new_story.append( '<button class="editStory" value="' + story.id +'" > Edit</button>' );
+				$new_story.append( '<button class="deleteStory" value="' + story.id +'" > Delete</button>' );				
+			}
+			if(story.user_id != current_user.id && current_user.likes.indexOf(story.id) == -1)
+				$new_story.append( '<button class="like" value="' + story.id +'" > Like </button>' );
+			//if(story.user_id != current_user.id && current_user.flags.indexOf(story.id) == -1)
+				//$new_story.append( '<button class="flag" value="' + story.id +'" > Mark as inappropriate </button>' );
+		}
 		//Append new story to the feed
 		$('#stories').append( $new_story );
 	}
